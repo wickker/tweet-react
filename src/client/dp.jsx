8f9c49963d47668ac01b89992a7cafcaecccf23b
wickker <choydianchun@gmail.com>
2020-05-28 20:56:05 +0800

    added user dp mouseover

--- src/client/dp.jsx
+++ src/client/dp.jsx
@@ -4,10 +4,22 @@ export default class Dp extends React.Component {
   render() {
     return (
       <div class="col-1">
-        <img class="dp"
-          src={this.props.tweet.user.profile_image_url}
-          alt="display picture"
-        ></img>
+        <a href="#" class="tip">
+          <img
+            class="dp"
+            src={this.props.tweet.user.profile_image_url}
+            alt="display picture"
+          ></img>
+          <div class="mouse-over">
+            <p>@{this.props.tweet.user.screen_name}</p>
+            <p>
+              <strong>{this.props.tweet.user.friends_count}</strong> Following
+            </p>
+            <p>
+              <strong>{this.props.tweet.user.followers_count}</strong> Followers
+            </p>
+          </div>
+        </a>
       </div>
     );
   }
